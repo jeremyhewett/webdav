a9a4bfd2bfc857e79965a70a51772d7ae366db60
Jeremy Hewett <jeremyhewett@gmail.com>
2018-01-02 11:04:51 -0800

    Different file types

--- client/client.js
+++ client/client.js
@@ -1,5 +1,13 @@
 angular.module('app', []).controller('filesController', ($http, $scope) => {
 
+  const appByExtension = {
+    '.doc': 'ms-word',
+    '.docx': 'ms-word',
+    '.xls': 'ms-excel',
+    '.xlsx': 'ms-excel',
+    '.pptx': 'ms-powerpoint'
+  };
+
   function init() {
     $http.get('api/files').then(({ data: files }) => {
       $scope.files = files;
@@ -10,7 +18,8 @@ angular.module('app', []).controller('filesController', ($http, $scope) => {
 
   $scope.openFile = (fileName) => {
     getToken().then(token => {
-      let href = `ms-word:ofe|u|${window.location.origin}/dav/token/${token}/${fileName}`;
+      let ext = fileName.substr(fileName.lastIndexOf('.'));
+      let href = `${appByExtension[ext]}:ofe|u|${window.location.origin}/dav/token/${token}/${fileName}`;
       let link = angular.element(`<a href="${href}">`);
       link[0].click();
     });
